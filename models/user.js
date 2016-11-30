8e806e43e2812ac64697188ed33e1549afebaab0
Dennis van Velzen <dvvelzen@xs4all.nl>
2016-11-30 11:20:12 +0100

    Add auth token generation to POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcryptjs = require('bcryptjs');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -23,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
 			validate: {
 				len: [7, 100]
 			},
-			set: function (value) {
+			set: function(value) {
 				var salt = bcryptjs.genSaltSync(10);
 				var hashedPassword = bcryptjs.hashSync(value, salt);
 
@@ -34,15 +36,15 @@ module.exports = function(sequelize, DataTypes) {
 		}
 	}, {
 		hooks: {
-			beforeValidate: function (user, options) {
-				if ( typeof user.email === 'string' ) {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
 		},
 		classMethods: {
-			authenticate: function (body) {
-				return new Promise(function (resolve, reject) {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
 					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
 						reject();
 					}
@@ -67,6 +69,24 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString(); 
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
+					
+					return token;
+				} catch (e) {
+					//console.error(e);
+					return undefined;
+				}
 			}
 		}
 	});
